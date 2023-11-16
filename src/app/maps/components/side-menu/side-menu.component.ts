5cd4ddc19aa277649063b0817700b6778ec9077a
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-15 20:49:19 -0500

    Add list side menu and interface

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { MenuItemI } from '../../interfaces/menu-item-i';
 
 @Component({
-  selector: 'maps-side-menu',
-  templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss']
+	selector: 'maps-side-menu',
+	templateUrl: './side-menu.component.html',
+	styleUrls: ['./side-menu.component.scss']
 })
 export class SideMenuComponent {
+	
+	public menuItems: MenuItemI[] = [
+		{ route: '/maps/fullscreen', 	name: 'Full Screen' },
+		{ route: '/maps/zoom-range', 	name: 'Zoom Range' },
+		{ route: '/maps/markers', 		name: 'Markers' },
+		{ route: '/maps/properties', 	name: 'Properties' },
+	];
 
 }
---
+++ src/app/maps/interfaces/menu-item-i.ts
@@ -0,0 +1,4 @@
+export interface MenuItemI {
+	name: string
+	route: string;
+}

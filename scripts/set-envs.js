87eedfc36090ac866af1544cace929a4088224ce
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-16 08:03:18 -0500

    Add config for environments

---
+++ scripts/set-envs.js
@@ -0,0 +1,19 @@
+// * fs = file system
+
+const { writeFileSync, mkdirSync } = require('fs');
+
+require('dotenv').config();
+
+const targetPath = './src/environments/environment.ts';
+
+const envFileContent = `
+
+   export const environment = {
+      MAPBOX_KEY: "${ process.env['MAPBOX_KEY'] }"
+   };
+
+`
+
+mkdirSync('./src/environments', { recursive: true });
+
+writeFileSync( targetPath, envFileContent );
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+
+   export const environment = {
+      MAPBOX_KEY: "pk.eyJ1IjoiamhvbmdvbWV6ZGV2ZWxvcGVyIiwiYSI6ImNscDE0Z3hyNTBmOTcya3Mxa3I2djE2ZHkifQ.DGS26MXR5om638CLv1mr5A"
+   };
+

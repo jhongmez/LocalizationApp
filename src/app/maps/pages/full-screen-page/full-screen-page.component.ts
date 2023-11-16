0aee0472c8cf6498975704fcb2b3b6147fbee603
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-16 15:48:58 -0500

    Add mapbox into component fullscreen

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
+// * debemos manejarlo de esta manera
+(mapboxgl as any).accessToken = 'pk.eyJ1IjoiamhvbmdvbWV6ZGV2ZWxvcGVyIiwiYSI6ImNscDE0NTNycTBkN3YybHM0dnBja3l4c2MifQ.t1sTVKRtVm2Jmqe3Hu8mQQ';
 
 @Component({
-  templateUrl: './full-screen-page.component.html',
-  styleUrls: ['./full-screen-page.component.scss']
+	templateUrl: './full-screen-page.component.html',
+	styleUrls: ['./full-screen-page.component.scss']
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
 
+	ngAfterViewInit(): void {
+
+		const map = new mapboxgl.Map({
+			container: 'map', // container ID
+			style: 'mapbox://styles/mapbox/streets-v12', // style URL
+			center: [-74.5, 40], // starting position [lng, lat]
+			zoom: 9, // starting zoom
+		});
+		
+	}
 }

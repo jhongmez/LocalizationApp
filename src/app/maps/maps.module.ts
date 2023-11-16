72cd34171c02147c466ffa72f88104b56ddc9ea9
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-15 20:35:09 -0500

    Modify name component

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MapsRoutingModule } from './maps-routing.module';
+
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
-import { SiteMenuComponent } from './components/site-menu/site-menu.component';
+
 import { MapsLayoutComponent } from './layout/maps-layout/maps-layout.component';
+
 import { FullScreenPageComponent } from './pages/full-screen-page/full-screen-page.component';
 import { MarkersPageComponent } from './pages/markers-page/markers-page.component';
 import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { ZoomRangePageComponent } from './pages/zoom-range-page/zoom-range-page.component';
 
 
 @NgModule({
   declarations: [
     MiniMapComponent,
-    SiteMenuComponent,
+    SideMenuComponent,
     MapsLayoutComponent,
     FullScreenPageComponent,
     MarkersPageComponent,

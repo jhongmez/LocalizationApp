d79d0e459bf4a9ffd5eedaf0a46cccffc27a4759
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-11-17 07:42:22 -0500

    Add functions zoomIn - zoomOut

--- src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -9,17 +9,50 @@ export class ZoomRangePageComponent implements AfterViewInit {
 	
 	@ViewChild('map') public divMap?: ElementRef;
 
+	public zoom: number = 10;
+	public map?: Map;
+
 	ngAfterViewInit(): void {
 
 		if( !this.divMap ) throw 'El HTML no fue encontrado (Mapa)';
 
-		const map = new Map({
+		this.map = new Map({
 			container: this.divMap.nativeElement, // container ID
 			style: 'mapbox://styles/mapbox/streets-v12', // style URL
 			center: [-74.5, 40], // starting position [lng, lat]
-			zoom: 9, // starting zoom
+			zoom: this.zoom, // starting zoom
 		});
 
+		this.mapsListeners();
+
+	}
+
+	mapsListeners() {
+		if ( !this.map ) throw 'Mapa no inicializado';
+
+		this.map.on('zoom', (ev) => {
+			this.zoom = this.map!.getZoom();
+		});
+
+		this.map.on('zoomend', (ev) => {
+			
+			if( this.map!.getZoom() < 17 ) return
+			this.map?.zoomTo(17)
+
+		})
+	}
+
+	zoomIn() {
+		this.map?.zoomIn();
+	}
+
+	zoomOut() {
+		this.map?.zoomOut();
+	}
+
+	zoomChanged( value: string ) {
+		this.zoom = Number(value);
+		this.map?.zoomTo( this.zoom );
 	}
 
 }
